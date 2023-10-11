7ba4ce56fab98b8ed919c253ddedd5d8d6f76539
lmontiel92 <146214091+lmontiel92@users.noreply.github.com>
2023-10-11 15:35:26 +0000

    final version

--- Think_Fast.rb
+++ Think_Fast.rb
@@ -9,10 +9,10 @@ unpredictable_inputs = [
   { :city => "Chicago", :state => "IL", :zip => 60654 }
 ]
 
-#some_random_input = unpredictable_inputs.sample
+some_random_input = unpredictable_inputs.sample
 # write your program below
 
-some_random_input = Time.now
+# some_random_input = Time.now # for testing puroposes
 
 class_type = some_random_input.class 
 
@@ -20,10 +20,31 @@ if class_type == String
   pp some_random_input.downcase
 
 elsif class_type == Time
-  pp some_random_input.strftime("%A")
+  pp some_random_input.strftime("%A").downcase
 
+elsif class_type == Integer
+  if some_random_input.even?
+    pp "#{some_random_input} is Even"
 
-else
-  pp "No String"
+  else
+    pp "#{some_random_input} is Odd"
+
+  end
+
+elsif class_type == Symbol
+  pp some_random_input.downcase
+
+elsif class_type == NilClass
+  pp "no object provided"
+
+elsif class_type == TrueClass
+  pp "you may pass"
+
+elsif class_type == FalseClass
+  pp "you may not pass"
+
+elsif class_type == Hash
+  pp some_random_input.keys
+
 end
   
